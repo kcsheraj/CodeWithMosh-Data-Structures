0d6202b0b9d52336781352812e76666f70f920d0
Sheraj <sherajkc@gmail.com>
2022-12-31 14:01:19 -0500

    Made method that reverses k elements in queue

--- Queues/QueueExercise.java
+++ Queues/QueueExercise.java
@@ -5,16 +5,6 @@ import java.util.Queue;
 import java.util.Stack;
 
 public class QueueExercise {
-    public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
-
-        reverse(queue);
-        
-
-    }
     public static void reverse(Queue<Integer> queue){//reverses queue
         //only allowed to use:add,remove,isEmpty methods
         Stack<Integer> stack = new Stack<>();
@@ -27,5 +17,41 @@ public class QueueExercise {
             queue.add(stack.pop());
         }
         
+    }
+
+    public static Queue<Integer> reverseFirstKElements(Queue<Integer> queue, int k){
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0; i<k;i++){//put first k elements into a stack
+            stack.add(queue.remove());
+        }
+        while(!stack.isEmpty()){//enqueue contents of stack at back of queue
+            queue.add(stack.pop());
+        }
+
+        for(int i=0; i< queue.size()-k; i++){//add the front of queue to back up to len-k
+            queue.add(queue.poll());
+        }
+
+        return queue;
+    }
+
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        reverse(queue);
+
+        Queue<Integer> queue2 = new ArrayDeque<>();
+        queue2.add(10);
+        queue2.add(20);
+        queue2.add(30);
+        queue2.add(40);
+        queue2.add(50);
+
+        queue2 = reverseFirstKElements(queue2,3);
+        
+        System.out.println();
     }
 }
