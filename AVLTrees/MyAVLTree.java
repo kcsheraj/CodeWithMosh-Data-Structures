4f70d3c3657f9bcce28bae89e88f6b7998507f40
Sheraj <sherajkc@gmail.com>
2023-01-10 13:51:37 -0500

    Implemented first approach to insert()

--- AVLTrees/MyAVLTree.java
+++ AVLTrees/MyAVLTree.java
@@ -4,11 +4,38 @@ public class MyAVLTree {
 
      //insert recursively
      public void insert(int value){
-        
+        insert(rootNode,value);
      }
 
+     private void insert(AVLNode node, int value){
+        if(node==null){
+            rootNode = new AVLNode(value);
+            return;
+        }
+        if(value>node.value){
+            if(node.rightChild==null){
+                node.rightChild = new AVLNode(value);
+                return;
+            }
+            insert(node.rightChild, value);
+        }
+        else{
+            if(node.leftChild==null){
+                node.leftChild = new AVLNode(value);
+                return;
+            }
+            insert(node.leftChild, value);
+        }
 
+    }
 
+    private boolean isLeaf(AVLNode node){
+        if(node.leftChild==null && node.rightChild==null){
+            return true;
+        }
+        return false;
+    }
+    
     private class AVLNode{
         private int value;
         private AVLNode leftChild;
@@ -19,6 +46,15 @@ public class MyAVLTree {
     }
      
     public static void main(String[] args) {
+        MyAVLTree tree1 = new MyAVLTree();
+
+        tree1.insert(7);
+        tree1.insert(8);
+        tree1.insert(4);
+        tree1.insert(5);
+        tree1.insert(23);
+        tree1.insert(21);
+        System.out.println();
         
     }
 }
