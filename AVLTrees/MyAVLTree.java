dc9e7d452a1da21395f8ec7b9fcf0c23fb04a148
Sheraj <sherajkc@gmail.com>
2023-01-09 22:58:27 -0500

    Starting AVLtrees

---
+++ AVLTrees/MyAVLTree.java
@@ -0,0 +1,24 @@
+package AVLTrees;
+public class MyAVLTree {
+    private AVLNode rootNode;
+
+     //insert recursively
+     public void insert(int value){
+        
+     }
+
+
+
+    private class AVLNode{
+        private int value;
+        private AVLNode leftChild;
+        private AVLNode rightChild;
+        public AVLNode(int value){
+            this.value = value;
+        }
+    }
+     
+    public static void main(String[] args) {
+        
+    }
+}
