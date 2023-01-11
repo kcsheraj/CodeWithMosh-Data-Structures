e6a762411ac5d1a7111aca0ae5745b5bcc03821a
Sheraj <sherajkc@gmail.com>
2023-01-11 14:42:49 -0500

    Created AVLTree datastruscure

--- AVLTrees/MyAVLTree.java
+++ AVLTrees/MyAVLTree.java
@@ -17,8 +17,68 @@ public class MyAVLTree {
         else{
             node.rightChild = insert(node.rightChild, value);
         }
-        return node;
 
+        setHeight(node);
+
+        return balance(node); 
+    }
+
+    private AVLNode balance(AVLNode node){
+        if(isLeftHeavy(node)){
+         if(balanceFactor(node.leftChild)<0){
+            node.leftChild = rotateLeft(node.leftChild);
+         }
+          return rotateRight(node);
+        }
+        else if (isRightHeavy(node)){
+            if(balanceFactor(node.rightChild)>0){//if needed right left rotation
+                node.rightChild = rotateRight(node.rightChild);
+            }
+           return rotateLeft(node.leftChild);//always left rotate if right heavy
+        }
+
+        return node;//if root was balanced
+    }
+
+    private AVLNode rotateLeft(AVLNode node){
+        AVLNode newRoot = node.rightChild;
+        node.rightChild = newRoot.leftChild;
+        newRoot.leftChild = node;
+        //reset height of the two nodes
+        setHeight(node);
+        setHeight(newRoot);
+
+        return newRoot;
+    }
+    private AVLNode rotateRight(AVLNode node){
+        AVLNode newRoot = node.leftChild;
+        node.leftChild = newRoot.rightChild;
+        newRoot.rightChild = node;
+        //reset height of the two nodes
+        setHeight(node);
+        setHeight(newRoot);
+
+        return newRoot;
+    }
+    private void setHeight(AVLNode node){
+        node.height = Math.max(height(node.leftChild), height(node.rightChild))+1;
+    }
+
+    private boolean isLeftHeavy(AVLNode node){
+        return balanceFactor(node) > 1;
+    }
+    private boolean isRightHeavy(AVLNode node){
+        return balanceFactor(node) < -1;
+    }
+
+    private int balanceFactor(AVLNode node){
+        return (node==null)? 0: height(node.leftChild) - height(node.rightChild);
+    }
+
+    private int height(AVLNode node){
+        if (node==null)
+            return -1;
+        return node.height;
     }
 
     private boolean isLeaf(AVLNode node){
@@ -32,6 +92,7 @@ public class MyAVLTree {
         private int value;
         private AVLNode leftChild;
         private AVLNode rightChild;
+        private int height;
         public AVLNode(int value){
             this.value = value;
         }
@@ -40,12 +101,10 @@ public class MyAVLTree {
     public static void main(String[] args) {
         MyAVLTree tree1 = new MyAVLTree();
 
-        tree1.insert(7);
-        tree1.insert(8);
-        tree1.insert(4);
+        tree1.insert(10);
+        tree1.insert(3);
         tree1.insert(5);
-        tree1.insert(23);
-        tree1.insert(21);
+
         System.out.println();
         
     }
